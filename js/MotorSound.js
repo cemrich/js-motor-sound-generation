6a2f4602342cf9ef91786ed4566fa1111646ede2
Christine Emrich <christine.emrich@web.de>
2013-12-10 00:19:24 +0100

    Moved generator into MotorSound module.

--- js/MotorSound.js
+++ js/MotorSound.js
@@ -1,10 +1,11 @@
 (function (exports) {
 
-	var MotorSound = function (context) {
+	var MotorSound = function (context, generator) {
 		this.currentFrame = 0;
 		this.context = context;
 		this.speed = 0.6;
 		this.isPlaying = false;
+		this.generator = generator;
 
 		// scriptNode to change sound wave on the run
 		this.scriptNode = context.createScriptProcessor(1024);
@@ -14,11 +15,13 @@
 		this.gainNode = context.createGain();
 		this.gainNode.gain.value = 0.5;
 		this.scriptNode.connect(this.gainNode);
+
+		this.regenerate();
+		this.gainNode.connect(this.context.destination);
 	};
 
-	MotorSound.prototype.setData = function (data) {
-		this.data = data;
-		this.gainNode.connect(this.context.destination);
+	MotorSound.prototype.regenerate = function (data) {
+		this.data = this.generator.generate();
 	};
 
 	MotorSound.prototype.setVolume = function (volume) {
@@ -43,6 +46,42 @@
 		}
 	};
 
+
+	var CanvasGenerator = function () {
+		this.canvas = document.createElement('canvas');
+		this.canvas.width = 1024;
+		this.canvas.height = 1;
+		this.ctx = this.canvas.getContext('2d');
+	};
+
+	CanvasGenerator.prototype.getRandomGradient = function () {
+		// get a horizontal gradient with several stops
+		var gradient = this.ctx.createLinearGradient(0, 0, this.canvas.width, 0);  
+		gradient.addColorStop(0, "rgba(0, 0, 0, 255)");
+		for (var i = 0.05; i < 1; i += Math.random()/8+0.01) {
+			gradient.addColorStop(i, "rgba(0, 0, 0," + Math.random() + ")");
+		}
+		gradient.addColorStop(1, "rgba(0, 0, 0, 255)");
+		return gradient;
+	};
+
+	CanvasGenerator.prototype.generate = function () {
+		// draw new gradient
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.ctx.fillStyle = this.getRandomGradient();
+		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+		// get data from gradient
+		var imageData = this.ctx.getImageData(0, 0, this.canvas.width, 1).data;
+		var data = [];
+		for (var i = 3, len = imageData.length; i < len; i += 4) {
+			data.push(imageData[i] / 128 - 1);
+		}
+		return data;
+	};
+
+
 	exports.MotorSound = MotorSound;
+	exports.CanvasGenerator = CanvasGenerator;
 
 })(window);
--- js/main.js
+++ js/main.js
@@ -3,41 +3,28 @@ window.onload = function () {
 	// canvas to generate gradients
 	var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext('2d');
-
 	// actual sound stuff
 	var AudioContext = window.AudioContext || window.webkitAudioContext;
 	var context = new AudioContext();
-	var motorSound = new MotorSound(context);
+	var generator = new CanvasGenerator();
+	var motorSound = new MotorSound(context, generator);
 
-	// get a horizontal gradient with several stops
-	function getRandomGradient() {
-		var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);  
-		gradient.addColorStop(0, "rgba(0, 0, 0, 255)");
-		for (var i = 0.05; i < 1; i += Math.random()/8+0.01) {
-			gradient.addColorStop(i, "rgba(0, 0, 0," + Math.random() + ")");
-		}
-		gradient.addColorStop(1, "rgba(0, 0, 0, 255)");
-		return gradient;
-	}
-
-	// make new random sound data
 	function regenerateSound() {
+		motorSound.regenerate();
+
 		// draw new gradient
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.fillStyle = getRandomGradient();  
-		ctx.fillRect(0, 0, canvas.width, canvas.height);
-		ctx.fillStyle = "#fff";
+		ctx.fillStyle = "#000";
 
-		// get data from gradient
-		var imageData = ctx.getImageData(0, 0, canvas.width, 1).data;
-		var data = [];
-		for (var i = 3, len = imageData.length; i < len; i += 4) {
-			var normalized = imageData[i] / 128 - 1;
-			data.push(normalized);
-			// draw data curve to canvas
-			ctx.fillRect(i/4, (1-normalized)*canvas.height/2, 1, 1);
+		// draw generated data to canvas
+		ctx.beginPath();
+		ctx.moveTo(0, (1 - motorSound.data[0]) * canvas.height / 2);
+		for (var i = 0, len = motorSound.data.length; i < len; i++) {
+			var value = motorSound.data[i];
+			ctx.lineTo(i/len*canvas.width, (1 - value) * canvas.height / 2);
 		}
-		motorSound.setData(data);
+		console.log(motorSound.data);
+		ctx.stroke();
 	}
 
 
